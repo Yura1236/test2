a18a65d599611f923a993801bc1dff079693e454
Yura1236 <126318963+Yura1236@users.noreply.github.com>
2024-06-13 19:58:15 +0300

    1

---
+++ script.js
@@ -0,0 +1,10 @@
+// This script will be used to handle dynamic functionalities like date filtering, etc.
+// You can expand this script based on the needed functionality
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loadMoreButton = document.querySelector('.load-more');
+    loadMoreButton.addEventListener('click', () => {
+        // Logic to load more posts goes here
+        alert('Load more posts');
+    });
+});
